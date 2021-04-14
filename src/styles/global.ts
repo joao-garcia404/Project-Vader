d2b3de5914cc317016d694b91ea4130ab64a2bfe
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2021-04-14 14:40:00 -0300

    Global styles created

---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --green: #00D369;
+
+    --input: #F0F0F0;
+
+    --title-dark: #000;
+    --text-dark: #000;
+    --text-white: #fff;
+
+    --background: #F1F2F7;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html { 
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased; 
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
